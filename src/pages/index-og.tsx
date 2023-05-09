591f256faac78470f97344c04b39ae550c1cbb03
loubnaStarton <115457891+loubnaStarton@users.noreply.github.com>
2023-05-09 12:48:03 +0200

    fix: fix broken links

--- src/pages/index-og.tsx
+++ src/pages/index-og.tsx
@@ -16,7 +16,7 @@ function HomepageHeader() {
 				<h1 className="hero__title">{siteConfig.title}</h1>
 				<p className="hero__subtitle">{siteConfig.tagline}</p>
 				<div className={styles.buttons}>
-					<Link className="button button--secondary button--lg" to="/docs/home">
+					<Link className="button button--secondary button--lg" to="/docs/overview">
 						Get started
 					</Link>
 				</div>
