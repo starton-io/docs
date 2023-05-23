6734d809b0b022535a8ff199dcdd941ca79360e6
Philippe Desplats <philippe@di-rupt.com>
2023-05-23 17:31:43 +0200

    chore(wip): try redirect plugin

--- config/plugins.config.js
+++ config/plugins.config.js
@@ -19,5 +19,19 @@ module.exports = {
 		// 		},
 		// 	],
 		// ]),
+
+		// Redirections plugin
+		// Link : https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-client-redirects
+		// ----------------------------------------------------------------------------
+		'@docusaurus/plugin-client-redirects',
+		{
+			redirects: [
+				// /docs/oldDoc -> /docs/newDoc
+				{
+					from: '/docs/Tutorials/Home',
+					to: '/tutorials',
+				},
+			],
+		},
 	],
 }
