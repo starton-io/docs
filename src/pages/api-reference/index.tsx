e8020a7eb09703b8be27453403ac3824a9c199ef
Philippe Desplats <philippe@di-rupt.com>
2023-10-31 09:35:18 +0100

    feat: update scalar api reference package (@marclave)

--- src/pages/api-reference/index.tsx
+++ src/pages/api-reference/index.tsx
@@ -15,7 +15,7 @@ import Layout from '@theme/Layout'
 export default function ApiReferencePage() {
 	React.useEffect(() => {
 		const scalarScript = document.createElement('script')
-		scalarScript.src = 'https://www.unpkg.com/@scalar/api-reference@0.8.4'
+		scalarScript.src = 'https://cdn.jsdelivr.net/npm/@scalar/api-reference@0.8.10'
 		scalarScript.async = true
 
 		scalarScript.onload = () => {
